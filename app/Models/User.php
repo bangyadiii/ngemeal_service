40624f2827e6610a9bd7bbc964e0265abb25765e
triadi <triadilaksamana@gmail.com>
2022-10-16 10:31:36 +0700

    add role model and m to n user role relational

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,10 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         $this->hasMany(Transactions::class);
     }
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_roles');
+    }
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ app/Models/UserRoles.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserRoles extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'role_id',
+    ];
+}
