66cbd0ab65542b7a4774b96ab3733d050ea23254
triadi <triadilaksamana@gmail.com>
2022-10-21 14:54:00 +0700

    spread route as its version

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticateUserController;
-use App\Http\Controllers\Auth\RegisterUserController;
-use App\Http\Controllers\Auth\UserInformationController;
-use App\Http\Controllers\FoodController;
-use App\Http\Controllers\StoreController;
-use App\Http\Controllers\TransactionController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,22 +20,6 @@ $routeAttributes = [
 ];
 
 Route::group($routeAttributes, function () {
-    Route::post("/login", [AuthenticateUserController::class, "login"])->name("login");
-    Route::post("/register", [RegisterUserController::class, "store"])->name("login");
-
-    Route::get("/foods", [FoodController::class, "index"])->name("get.food");
-    Route::post("/checkout", [TransactionController::class, "checkout"])->name("checkout");
-
-    // auth sanctum
-    Route::group(["middleware" => "auth:sanctum"], function () {
-        Route::as("auth.")->prefix("auth")->group(function () {
-            Route::put("/user/update", [UserInformationController::class, "update"])->name("update");
-            Route::get("/me", [UserInformationController::class, "show"])->name("show");
-        });
-
-        Route::apiResource("store", StoreController::class)->except("index", "show");
-        Route::apiResource("foods", FoodController::class)->except("index", "show");
-        Route::get("/transactions", [TransactionController::class, "all"])->name("get.transaction");
-        Route::put("/transactions/{id}", [TransactionController::class, "update"])->name("update.transaction");
-    });
+    //
+    require __DIR__ . '/api/v1.php';
 });
---
+++ routes/api/v1.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticateUserController;
+use App\Http\Controllers\Auth\RegisterUserController;
+use App\Http\Controllers\Auth\UserInformationController;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\StoreController;
+use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::post("/login", [AuthenticateUserController::class, "login"])->name("login");
+Route::post("/register", [RegisterUserController::class, "store"])->name("login");
+
+Route::get("/foods", [FoodController::class, "index"])->name("get.food");
+Route::post("/checkout", [TransactionController::class, "checkout"])->name("checkout");
+
+// should authenticated
+Route::group(["middleware" => "auth:sanctum"], function () {
+    Route::as("auth.")->prefix("auth")->group(function () {
+        Route::put("/user/update", [UserInformationController::class, "update"])->name("update");
+        Route::get("/me", [UserInformationController::class, "show"])->name("show");
+    });
+
+    Route::apiResource("store", StoreController::class)->except("index", "show");
+    Route::apiResource("foods", FoodController::class)->except("index", "show");
+    Route::get("/transactions", [TransactionController::class, "all"])->name("get.transaction");
+    Route::put("/transactions/{id}", [TransactionController::class, "update"])->name("update.transaction");
+});
